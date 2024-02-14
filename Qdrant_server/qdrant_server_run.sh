7670aea3468eb5c00aa9890315fc2020c5fc2344
Le-Quoc-Trung0304 <75361949+Le-Quoc-Trung0304@users.noreply.github.com>
2024-02-14 15:04:22 +0700

    Update qdrant_server_run.sh

--- Qdrant_server/qdrant_server_run.sh
+++ Qdrant_server/qdrant_server_run.sh
@@ -1,4 +1,4 @@
 sudo docker pull qdrant/qdrant 
 sudo docker network create vector-database-network
 sudo docker run --network vector-database-network --name qdrant-container -p 6-333:6333 -v /home/ubuntu/qdrant_server/:/qdrant/storage -d qdrant/qdrant
-sudo docker run --network vector-database-network -p 80:80 sentence-embedding-model:latest_2
+sudo docker run --network vector-database-network -p 80:80 -v /home/ubuntu/temp_data/:/home/ubuntu/temp_data/ sentence-embedding-model:latest_2
